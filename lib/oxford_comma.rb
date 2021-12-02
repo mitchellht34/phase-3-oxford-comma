1c4fecc117440023e93e8c0281ff572d94276b36
Mitch Toran <mitchellht34@gmail.com>
2021-12-02 18:02:20 -0500

    commit tree 0ebf58f17a5e240b21a7d5c312587a4ca483b0ce

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length > 2
+        seperator = ", "
+    else
+        seperator = " "
+    end
+    if array.length != 1
+        array[-1] = "and #{array[-1]}"
+    end
+    array.join(seperator)
 end
